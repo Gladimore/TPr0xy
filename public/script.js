25d6cd08d39090635587013d8381aeb583c338d1
Gladimore <105724638+Gladimore@users.noreply.github.com>
2023-12-15 14:34:10 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,72 +1,45 @@
-var haha = document.querySelector("#haha");
-var lol = []
+document.body.visibility = "hidden";
 
-fetch('./assets/sprites.json')
-.then((res) => res.json())
-.then((data) => {
-  lol = data
-  shuffle(lol)
-  lmao();
-})
+const pass = prompt("Enter Password:");
 
-async function getRandomJoke() {
-  const response = await fetch('https://api.chucknorris.io/jokes/random');
-  const joke = await response.json();
-  lol.push(joke.value)
+if (pass.toLowerCase() === "LuccasMad".toLowerCase()) {
+    document.body.visibility = "visible";
 }
-getRandomJoke();
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex > 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
-let jokeOn = 0
-function lmao(){
-  jokeOn++;
-
-  if (jokeOn < lol.length){
-    haha.textContent = lol[jokeOn];
-  } else{
-     jokeOn = 0;
-    shuffle(lol) 
-    haha.textContent = lol[jokeOn]
-  }
-}
-
-
-lmao();
-
-haha.addEventListener('click', function(){
-  lmao();
-});
-
 document.body.addEventListener("keydown", (event) => {
-  if (event.keyCode == 13) {
+  if (event.key == 'Enter') {
     getSite();
   }
 });
 
 function getSite() {
-  let inputValue = document.querySelector('input').value;
+  document.querySelector(".overlay").style.visibility = "visible";
+  let inputValue = document.querySelector("input").value;
 
-  if (!inputValue.startsWith('http://') && !inputValue.startsWith('https://')) {
-    inputValue = 'http://' + inputValue;
+  if (!inputValue.startsWith("http://") && !inputValue.startsWith("https://")) {
+    inputValue = "http://" + inputValue;
   }
 
   const encodedValue = btoa(inputValue);
-  window.location.href = "/web/_" + encodedValue + '_/';
+  window.location.href = "/web/_" + encodedValue + "_/";
 }
