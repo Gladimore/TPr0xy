ed5dfb921090dc722c4865f1b4b9c31046d2498e
Gladimore <105724638+Gladimore@users.noreply.github.com>
2023-12-18 10:55:54 -0600

    Update blank.js

--- public/blank.js
+++ public/blank.js
@@ -7,18 +7,12 @@ function blank(){
   '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="'+path+'"></iframe>');
 }
 
-
-const pass = prompt("Enter Password:") || "";
-
-if (pass.toLowerCase() === "Im Not Sharing".toLowerCase()) {
-  if (window.self === window.parent){  
-    blank();
-    location.href = "//google.com"
-  } else {
-    if (location.pathname !== path){
-      location.href = path
-    }
+if (window.self === window.parent){  
+  blank();
+  location.href = "//google.com"
+} else {
+  if (location.pathname !== path){
+    location.href = path
   }
-} else{
-  location.href = '/html/blank.html'
 }
+
